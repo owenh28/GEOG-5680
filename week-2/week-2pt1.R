242051e9a3719a60c5620b2189655d12873d48ad
owenh <owenharlacker@outlook.com>
2023-05-21 20:29:05 -0600

    workin on week 2 stuffs

---
+++ week-2/week-2pt1.R
@@ -0,0 +1,17 @@
+# Data Input and data general info about data
+fluDeaths <- read.csv('./week-2/BirdFlu_deaths.csv')
+fdNames <- names(fluDeaths)
+fdHead <- head(fluDeaths)
+fdStr <- str(fluDeaths)
+
+# Find max fluDeaths in the year 2005 and store it, then find position of that number and the associated country
+fd2005 <- max(fluDeaths$yr2005)
+fdRow2005 <- which(fluDeaths$yr2005 == fd2005)
+maxCountry2005 <- fluDeaths[fdRow2005, which(fdNames == 'Country')]
+maxCountry2005
+
+# Same here but for year 2007
+fd2007 <- max(fluDeaths$yr2007)
+fdRow2007 <- which(fluDeaths$yr2007 == fd2007)
+maxCountry2007 <- fluDeaths[fdRow2007, which(fdNames=='Country')]
+maxCountry2007
